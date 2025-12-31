0e29f293825ea84d73e4b896c1d5b0973720d78c
gtkrshnaaa <gtkrshnaaa@gmail.com>
2025-12-31 16:17:46 +0700

    configure routes for public portfolio pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Public\PortfolioController;
 
+// Public Portfolio Routes
+Route::prefix('portfolio')->name('public.')->group(function () {
+    Route::get('/', [PortfolioController::class, 'show'])->name('home');
+});
+
+// Redirect root to portfolio
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('public.home');
 });
